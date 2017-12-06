6b1c4d29d476c3e0fccfbdb19c101f3d5ec5ad90
Zeyla Hellyer <zey@zey.moe>
2017-12-05 18:00:32 -0800

    Add more tests

--- tests/pairings.rs
+++ tests/pairings.rs
@@ -19,3 +19,26 @@ fn comments() {
     assert_eq!(utils::parse_line("key#=value"), None);
     assert_eq!(utils::parse_line("key=#abc"), Some(("key", "")));
 }
+
+#[test]
+fn only_keys() {
+    let lines = utils::parse_lines("KEY=value\nKEY2=value2");
+    let mut vec = Vec::with_capacity(lines.len());
+    utils::only_keys(&lines, &mut vec);
+
+    assert_eq!(vec, &["KEY", "KEY2"]);
+}
+
+#[test]
+fn parse_line() {
+    assert_eq!(utils::parse_line("KEY=value"), Some(("KEY", "value")));
+    assert_eq!(utils::parse_line("KEY=value#test"), Some(("KEY", "value")));
+    assert!(utils::parse_line("KEY").is_none());
+    assert_eq!(utils::parse_line("KEY="), Some(("KEY", "")));
+    assert!(utils::parse_line("KEY#B=C#").is_none());
+}
+
+#[test]
+fn parse_lines() {
+    assert_eq!(utils::parse_lines("A=B\nC=D\nE=F#").len(), 3);
+}
