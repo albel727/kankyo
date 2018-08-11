9a9a980cccdb33df3accb48bb9d4c8a08e4d41f1
Zeyla Hellyer <hi@zeyla.me>
2018-08-10 21:12:03 -0700

    Fix race condition in tests
    Tests are run in their own threads, which can set environment variables. Other
    threads could expect the state of the environment variables to be something that
    they previously set but was overridden by another test.
    
    This puts all of the utils tests in one test.

--- tests/utils.rs
+++ tests/utils.rs
@@ -4,7 +4,7 @@ use kankyo::utils::*;
 use std::env;
 
 #[test]
-fn test_set_variables() {
+fn test_utils() {
     set_variables(&[("foo", "1"), ("bar", "2")]);
     assert!(env::var("foo").is_ok());
 
@@ -12,19 +12,15 @@ fn test_set_variables() {
     assert!(env::var("bar").is_ok());
     unload(&["bar"]);
     assert!(env::var("bar").is_err());
-}
 
-#[test]
-fn test_unload() {
+    // test unload fn
     env::set_var("foo", "1");
     assert!(env::var("foo").is_ok());
 
     unload(&["foo"]);
     assert!(env::var("foo").is_err());
-}
 
-#[test]
-fn test_unload_from_parsed_lines() {
+    // test unload from parsed_lines
     env::set_var("foo", "1");
     assert!(env::var("foo").is_ok());
 
